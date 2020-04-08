bf084e3dda0fe9c38dba67117c2663fddbbd3fa6
Julian Lu <j.lu@student.fontys.nl>
2020-04-08 13:32:00 +0200

    Added lunch functionality, still needs to be connected with the api

--- src/components/RegisterLunch.js
+++ src/components/RegisterLunch.js
@@ -1,31 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState, Component } from 'react'
 import { Button } from "react-bootstrap";
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
 import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
 
 import '../css/RegisterLunch.css'
 function RegisterLunch() {
-    const events = [{ title: "Event van vandaag", date: new Date() }];
 
+    const [lunch, setLunch] = useState([
+        {
+            title: "Lunch",
+            date: new Date("2020-04-10")
+        }
+    ])
 
-    function addlunch(lunchDate) {
-        fetch('accounts/' + window.sessionStorage.getItem("userId") + '/lunches', {
+    function getLunches() {
+        const url = 'http://localhost:8090/accounts/' + window.sessionStorage.getItem("userId") + '/lunches'
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + window.sessionStorage.getItem("token")
+            }
+        })
+            .then(res => res.json()).catch()
+            .then((data) => {
+                setLunch(data)
+            })
+    }
+
+    function addlunchApi(lunch) {
+        fetch('http://localhost:8090/accounts/' + window.sessionStorage.getItem("userId") + '/lunches', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                date: lunchDate,
-                name: "Test"
+                name: lunch.title,
+                date: lunch.date
             })
-        }).then(
-            window.alert("De lunch is succesvol toegevoegd!")
-        )
+        }).then(() => {
+            window.alert("De lunch is succesvol toegevoegd!");
+        })
     }
+
 
     function addToday() {
         let today = new Date();
@@ -34,7 +58,27 @@ function RegisterLunch() {
         let yyyy = today.getFullYear();
 
         today = mm + '/' + dd + '/' + yyyy;
-        addlunch(today);
+
+        const date = new Date(today);
+
+        const newLunch = {
+            title: "Lunch",
+            date: date
+        }
+        setLunch(oldLunches => [...oldLunches, newLunch]);
+        //addlunchApi(lunch);
+    }
+
+    // Wanneer er op een datum wordt geklikt
+    const handleDateClick = arg => {
+        const date = new Date(arg.dateStr);
+        const newLunch = {
+            title: "Lunch",
+            date: date
+        }
+        // addlunchApi(lunch)
+        setLunch(oldLunches => [...oldLunches, newLunch]); // Dit update de lunch array , Wanneer dit samen werkt met de api, kunnen we ook alle lunches ophalen en die in de array zetten
+        console.log(lunch);
     }
 
     return (
@@ -43,10 +87,11 @@ function RegisterLunch() {
             <p>Weekoverzicht</p>
             <FullCalendar
                 defaultView="dayGridWeek"
-                plugins={[dayGridPlugin]}
-                events={events}
+                plugins={[dayGridPlugin, interactionPlugin]}
+                events={lunch}
                 locale="nl"
                 contentHeight='auto'
+                dateClick={handleDateClick}
             />
             <Button block size="lg" variant="success" type="submit" onClick={addToday}>
                 Ik heb vandaag meegeluncht
@@ -57,3 +102,23 @@ function RegisterLunch() {
 }
 
 export default RegisterLunch
+
+
+
+// function addTomorrow() {
+//     let d = new Date("2020-04-10")
+//     let a = new Date("2020-04-11")
+
+    // let datums = [
+    //     {
+    //         title: "Lunch",
+    //         date: d
+    //     },
+    //     {
+    //         title: "Lunch",
+    //         date: a
+    //     }
+    // ]
+
+//     setLunch(datums)
+// }
