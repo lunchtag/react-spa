45f1f7ed4a493a7e71aa35585737a51a22388a8b
Mak,Niray N.P.F <niray11@live.nl>
2020-04-21 10:14:28 +0200

    navbar is now fixed for scrollable pages

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,7 @@ import "./navbar.css";
 class Navbar extends React.Component {
 	render() {
 		return (
-			<div>
+			<div class="navigationpanel">
 				<div className="welcome">Welkom</div>
 
 				<nav>
@@ -13,6 +13,7 @@ class Navbar extends React.Component {
 					<Link to="/add"> Lunch weekoverzicht</Link>
 					<Link to="/lunch">Lunch overview</Link>
 
+					<Link to="/secaddlunch">Secretaresse add lunch</Link>
 					<Link to="/logout">Logout</Link>
 				</nav>
 			</div>
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,28 +1,42 @@
-import React from 'react';
-import '../css/App.css';
-import { BrowserRouter, Switch } from 'react-router-dom';
-import PublicRoute from '../components/PublicRoute'
-import Login from './Login.js'
-import UserLunchOverView from './UserLunchOverView'
-import Register from './Register'
-import LunchOverview from '../secPages/LunchOverview'
-import Navbar from '../components/navbar/navbar.js'
-import RegisterLunch from '../components/RegisterLunch';
+import React from "react";
+import "../css/App.css";
+import { BrowserRouter, Switch } from "react-router-dom";
+import PublicRoute from "../components/PublicRoute";
+import Login from "./Login.js";
+import UserLunchOverView from "./UserLunchOverView";
+import Register from "./Register";
+import LunchOverview from "../secPages/LunchOverview";
+import Navbar from "../components/navbar/navbar.js";
+import RegisterLunch from "../components/RegisterLunch";
+import SecAddLunch from "../secPages/AddLunch";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <PublicRoute restricted={false} component={Login} path="/" exact></PublicRoute>
-          <PublicRoute path="/nav" component={Navbar}></PublicRoute>
-          <PublicRoute restricted={false} component={UserLunchOverView} path="/dashboard" exact></PublicRoute>
-          <PublicRoute path="/lunch" component={LunchOverview}></PublicRoute>
-          <PublicRoute path="/register" component={Register}></PublicRoute>
-          <PublicRoute path="/add" component={RegisterLunch}></PublicRoute>
-        </Switch>
-      </BrowserRouter>
-    )
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch>
+					<PublicRoute
+						restricted={false}
+						component={Login}
+						path="/"
+						exact
+					></PublicRoute>
+					<PublicRoute path="/nav" component={Navbar}></PublicRoute>
+					<PublicRoute
+						restricted={false}
+						component={UserLunchOverView}
+						path="/dashboard"
+						exact
+					></PublicRoute>
+					<PublicRoute path="/lunch" component={LunchOverview}></PublicRoute>
+					<PublicRoute path="/register" component={Register}></PublicRoute>
+					<PublicRoute path="/add" component={RegisterLunch}></PublicRoute>
+					<PublicRoute
+						parth="/secaddlunch"
+						component={SecAddLunch}
+					></PublicRoute>
+				</Switch>
+			</BrowserRouter>
+		);
+	}
 }
-
