a31164b738e248e1073669b506ef1870610293e2
Mak,Niray N.P.F <niray11@live.nl>
2020-06-10 14:06:09 +0200

    added warning message if add lunch fails on UserLunchOverView page

--- src/pages/UserLunchOverView.js
+++ src/pages/UserLunchOverView.js
@@ -108,6 +108,12 @@ class UserLunchOverView extends React.Component {
 						showMessage: true,
 						message: "Lunch succesvol toegevoegd",
 					});
+				} else {
+					this.setState({
+						messageType: "warning",
+						showMessage: true,
+						message: "Er is iets misgegaan",
+					});
 				}
 			});
 		};
