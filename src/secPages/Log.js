65da924ffc3e54b27faa1fb324896ea13df1a43a
Odin van der Linden <odinvanderlinden2001@gmail.com>
2020-06-10 13:43:23 +0200

    materialUI

--- src/secPages/Log.js
+++ src/secPages/Log.js
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React  from "react";
 import Navbar from "../components/navbar/navbar";
-import Table from "react-bootstrap/Table";
 
 import Pagination from '@material-ui/lab/Pagination';
 import { getAllLogs } from "../service/logService";
 import "../css/log.css";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import {Container} from "@material-ui/core"
 
-
-
-export default class Log extends React.Component {
+const useStyles = makeStyles({
+	table: {
+	  minWidth: 650,
+	},
+  });
+class Log extends React.Component {
 	constructor(props) {
 		super(props);
 
@@ -33,7 +42,7 @@ export default class Log extends React.Component {
 	}
 
 	handleChange = (event, value) => {
-		this.state.currentPage = value;
+		this.setState({ currentPage: value });
 		this.makeSubArray();
 	}
 
@@ -50,41 +59,41 @@ export default class Log extends React.Component {
 
 	render() {
 		const logs = this.state.logs;
-
+		const { classes } = this.props;
 		return (
 			<div className="flexboxes">
 				<Navbar />
 				<div className="rightpanel">
 					<h1>Audit logs</h1>
-
-					<Table striped bordered hover>
-						<thead>
-							<tr>
-								<th>User</th>
-								<th>Tijd gemaakt</th>
-								<th>Log omschrijving</th>
-							</tr>
-						</thead>
-						<tbody>
-
-							{this.state.subLogs.map((item) => (
-								<tr>
-									<td>{item.user.name + " " + item.user.lastName}</td>
-
-									<td>
-										{new Date(item.dateOfLog).toLocaleDateString() +
+					<Container maxWidth="md">
+						<Table className={classes.table} aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>User</TableCell>
+									<TableCell align="right">Tijd gemaakt</TableCell>
+									<TableCell align="right">Log omschrijving</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{this.state.subLogs.map((row) => (
+									<TableRow key={row.name}>
+										<TableCell component="th" scope="row">
+											{row.user.name}
+										</TableCell>
+										<TableCell align="right">{new Date(row.dateOfLog).toLocaleDateString() +
 											" " +
-											new Date(item.dateOfLog).toLocaleTimeString()}
-									</td>
-
-									<td>{item.logText}</td>
-								</tr>
-							))}
-						</tbody>
-					</Table>
+											new Date(row.dateOfLog).toLocaleTimeString()}</TableCell>
+										<TableCell align="right">{row.logText}</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</Container>
 					<Pagination onChange={this.handleChange} className="pagination" count={this.state.count} />
 				</div>
 			</div>
 		);
 	}
 }
+
+export default withStyles(useStyles)(Log);
